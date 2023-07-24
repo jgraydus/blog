e6c91fccfe1e511cbc104d84ee3874733f7b5545
Josh <josh@grayd.us>
2023-07-24 13:21:58 +0900

    fix what seems to be a typo

--- client/src/components/md-editor.jsx
+++ client/src/components/md-editor.jsx
@@ -81,7 +81,7 @@ export default ({ onSave, initialValue, editable = true }) => {
                     PreTag="div"
                   />
                 ) : (
-                  <code {...profile} className={className}>
+                  <code {...props} className={className}>
                     {children}
                   </code>
                 );
